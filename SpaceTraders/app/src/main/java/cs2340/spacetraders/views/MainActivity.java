04c78dbda45ad564f043f5fc3b4dcd8decd97179
Gabriel Wang <43443158+gabriel-q-wang@users.noreply.github.com>
2019-02-16 17:48:32 -0500

    Reverted changed

--- SpaceTraders/app/src/main/java/cs2340/spacetraders/views/MainActivity.java
+++ SpaceTraders/app/src/main/java/cs2340/spacetraders/views/MainActivity.java
@@ -13,9 +13,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
-
 import cs2340.spacetraders.R;
+
 import cs2340.spacetraders.entity.Difficulty;
+import cs2340.spacetraders.entity.Player;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,6 +31,46 @@ public class MainActivity extends AppCompatActivity {
         adapter_standing.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         difficultySpinner.setAdapter(adapter_standing);
 
+        Player testPlayer = new Player("Test Player", 1,2,3,4);
+        Log.d("PlayerTesting",testPlayer.toString());
     }
 
+    public void onPlayPressed(View view) {
+        Log.d("Config", "Okay Button Pressed");
+
+        TextInputEditText nameInput = findViewById(R.id.playerNameInput);
+        EditText engineerInput = findViewById(R.id.engineerStat);
+        EditText fighterInput = findViewById(R.id.fighterStat);
+        EditText traderInput = findViewById(R.id.traderStat);
+        EditText pilotInput = findViewById(R.id.pilotStat);
+
+
+        String name = nameInput.getText().toString();
+        if (nameInput.getText().toString().length() == 0) {
+            Log.d("Config", "Name is Empty");
+            Toast.makeText(getApplicationContext(), "Name is Empty", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        int engineerStat, fighterStat, traderStat, pilotStat;
+        try {
+            engineerStat = Integer.parseInt(engineerInput.getText().toString());
+            fighterStat = Integer.parseInt(fighterInput.getText().toString());
+            traderStat = Integer.parseInt(traderInput.getText().toString());
+            pilotStat = Integer.parseInt(pilotInput.getText().toString());
+        } catch (Exception E) {
+            Log.d("Config", "Invalid Skill Point Inputs");
+            Toast.makeText(getApplicationContext(), "Invalid Skill Points Input", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if (engineerStat + fighterStat + traderStat + pilotStat != 16) {
+            Log.d("Config", "Skill Point aren't equal to 16");
+            Toast.makeText(this, "Skill Points aren't equal to 16", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        Log.d("Config", "Start Game!");
+        Toast.makeText(this, "Start Game!", Toast.LENGTH_LONG).show();
+    }
 }
