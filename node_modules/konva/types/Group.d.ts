c2a68c0f267ce4e1bd48002f9fd1453ed6034235
Varnika <varnikabudati@gmail.com>
2019-03-24 15:55:56 -0400

    added the navigation menu

---
+++ node_modules/konva/types/Group.d.ts
@@ -0,0 +1,4 @@
+import { Container } from './Container';
+export declare class Group extends Container {
+    _validateAdd(child: any): void;
+}
---
+++ node_modules/konva/types/Validators.d.ts
@@ -0,0 +1,9 @@
+export declare function RGBComponent(val: any): number;
+export declare function alphaComponent(val: any): any;
+export declare function getNumberValidator(): (val: any, attr: any) => any;
+export declare function getNumberOrAutoValidator(): (val: any, attr: any) => any;
+export declare function getStringValidator(): (val: any, attr: any) => any;
+export declare function getFunctionValidator(): (val: any, attr: any) => any;
+export declare function getNumberArrayValidator(): (val: any, attr: any) => any;
+export declare function getBooleanValidator(): (val: any, attr: any) => any;
+export declare function getComponentValidator(components: any): (val: any, attr: any) => any;
