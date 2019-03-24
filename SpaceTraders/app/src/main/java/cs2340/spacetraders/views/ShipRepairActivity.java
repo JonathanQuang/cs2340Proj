4cec2274bd3203423e4108d114816bbe5c168967
Varnika <varnikabudati@gmail.com>
2019-03-24 16:13:13 -0400

    adding more menu stuff which i accidentally deleted

---
+++ SpaceTraders/app/src/main/java/cs2340/spacetraders/views/ShipRepairActivity.java
@@ -0,0 +1,27 @@
+package cs2340.spacetraders.views;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import cs2340.spacetraders.R;
+
+public class ShipRepairActivity extends AppCompatActivity {
+    private FloatingActionButton menuButton;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ship_repair_screen);
+
+        menuButton = findViewById(R.id.menuButton);
+
+        menuButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(ShipRepairActivity.this, MenuScreen.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+    }
+}
